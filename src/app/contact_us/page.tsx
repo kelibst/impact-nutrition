3573659c24cb105c75a955678fcd33f28644fdef
kelibst <kbooster17@gmail.com>
2023-04-30 23:26:28 +0000

    setup backend

--- src/app/contact_us/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const contact_us = () => {
-  return <div>contact_us</div>;
-};
-
-export default contact_us;
--- src/lib/getPosts.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "./prisma";
-
-const getPosts = async () => {
-  const feed = await prisma.post.findMany();
-
-  return feed;
-};
-
-export default getPosts;
