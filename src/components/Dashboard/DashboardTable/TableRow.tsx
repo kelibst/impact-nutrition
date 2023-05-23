47281faa6db2df8f9b6fe5d5e4b1c8f4811d927f
kelibst <kbooster17@gmail.com>
2023-05-23 09:08:43 +0000

    merge main

--- src/components/Dashboard/DashboardTable/TableRow.tsx
+++ src/components/Dashboard/DashboardTable/TableRow.tsx
@@ -3,7 +3,10 @@ import { capitalizeWord } from "@/app/utils/capitalizeWord";
 import RowStatus from "./RowStatus";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { useState } from "react";
+<<<<<<< HEAD
 import Image from "next/image";
+=======
+>>>>>>> 43bbbb2 (add dashboard table and custom 404 page (#9))
 
 type RowDetail = {
   name: string;
