9104a019f54fcc0b19296dbe1067a9f2a868ae87
kelibst <kbooster17@gmail.com>
2023-05-27 18:10:12 +0000

    update fetch post useEffects

--- src/components/Dashboard/DashboardPost/index.tsx
+++ src/components/Dashboard/DashboardPost/index.tsx
@@ -1,10 +1,10 @@
 'use client'
 import { useState, useEffect, useRef } from 'react';
-import getPosts from '@/lib/getPosts';
 import 'suneditor/dist/css/suneditor.min.css';
 import UpdatePost from './UpdatePost';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import getPosts from '@/lib/getPosts';
 
 export interface Post {
     id?: string;
